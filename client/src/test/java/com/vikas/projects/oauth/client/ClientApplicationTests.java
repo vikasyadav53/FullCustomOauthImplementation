46551d5fd7754ed921f3a891f27ee36194508c22
Vikas Yadav <vikasyadav.a079@gmail.com>
2020-06-14 16:19:09 +0530

    Client implementation part 1

---
+++ client/src/test/java/com/vikas/projects/oauth/client/ClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vikas.projects.oauth.client;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
